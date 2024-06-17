09a92eb999a4a52936900c3ed17072381eb7c067
Ola Berglund <olakjberglund@gmail.com>
2024-06-17 23:04:42 +0200

    status api; ToLine class; error handling

--- app/Main.hs
+++ app/Main.hs
@@ -7,18 +7,19 @@
 
 module Main where
 
-import           Control.Exception    (IOException, catch)
-import           Control.Monad        (void)
+import           Control.Monad        (void, (>=>))
 import           Data.Aeson           (FromJSON (parseJSON), ToJSON (toJSON),
-                                       decode, object, withObject, (.:), (.=))
+                                       decode, object, withObject, withText,
+                                       (.:), (.=))
 import qualified Data.ByteString.Lazy as LBS
 import           Data.Coerce          (coerce)
+import           Data.List            (intersperse, sort)
 import           Data.List.NonEmpty   (NonEmpty)
 import qualified Data.List.NonEmpty   as NonEmpty
-import           Data.Maybe           (fromMaybe)
+import           Data.Maybe           (fromMaybe, mapMaybe)
+import           Data.Ord             (comparing)
 import           Data.Text            (Text)
 import qualified Data.Text            as Text
-import qualified Data.Text.IO         as Text.IO
 import           GHC.Generics         (Generic)
 import           Network.HTTP.Client  (defaultManagerSettings, newManager)
 import           Prelude              hiding (log)
@@ -29,15 +30,17 @@ import           Servant.Client       (AsClientT, ClientEnv, ClientError (..),
                                        ClientM, Response, client, mkClientEnv,
                                        parseBaseUrl, responseBody, runClientM,
                                        (//))
-import           Turtle               (Line, MonadIO (liftIO), Shell, die, echo,
-                                       export, inproc, lineToText, select, sh,
-                                       textToLines, unsafeTextToLine, (<&>))
-import           Turtle.Line          (textToLine)
+import           Turtle               (Line, MonadIO (liftIO), Shell, UTCTime,
+                                       die, inproc, lineToText, select, sh,
+                                       textToLine, textToLines)
 
 default (Text)
 
-dmenu :: [Text.Text] -> Shell Line -> Shell Line
-dmenu = inproc "dmenu"
+class ToLine a where
+    toLine :: a -> Line
+
+dmenuSelect :: [Text.Text] -> NonEmpty Line -> Shell Line
+dmenuSelect args ls = inproc "dmenu" (["-l", Text.pack (show (length ls))] <> args) (select ls)
 
 main :: IO ()
 main = do
@@ -45,24 +48,36 @@ main = do
     baseUrl <- parseBaseUrl "http://localhost:8087"
     let env = mkClientEnv manager baseUrl
     sh $ do
-        _pw <- askPassword
-        (UnlockData _title key) <- login env (Password "")
-        case textToLine key of
-            Nothing   -> die "Failed to parse key"
-            Just key' -> echo "" -- export "BW_SESSION" (lineToText key')
-        items <- coerce (getItems env)
-        let options = NonEmpty.fromList $ map itemToLine items <> ["Log out"]
-        dmenuSelect options >>= \case
-            "Log out" -> logout env <&> dmenuSelect . pure . unsafeTextToLine . coerce
-            something -> inproc "dunstify" ["-t", "1000", lineToText something] "" >> die (lineToText something)
+        status <- getStatus env
+        case statusDataStatus status of
+            Locked -> do
+                _pw <- askPassword
+                _ <- login env (Password "")
+                die "Logged in"
+            Unlocked -> do
+                items <- sort <$> getItems env
+                let options = NonEmpty.fromList $ map toLine items <> otherActions
+                selected <- dmenuSelect [] options
+                liftIO $ print selected
+
+data MetaAction = Cancel | LogOut
+    deriving stock (Show, Eq, Enum, Bounded)
+
+instance ToLine MetaAction where
+    toLine = \case
+        LogOut -> "Lock vault"
+        Cancel -> "Cancel"
+
+otherActions :: [Line]
+otherActions = map toLine [minBound :: MetaAction ..]
 
 handleClientError :: ClientError -> Shell a
 handleClientError clientError = case clientError of
-    (DecodeFailure df _)           -> announce "Decode failure" *> die df
-    (ConnectionError _)            -> (announce *> die) "Connection error"
+    (DecodeFailure df _)           -> announce "Decode failure" >> die df
+    (ConnectionError _)            -> logId "Connection error"
     (UnsupportedContentType _ res) -> log "Unsupported content type" res
     (InvalidContentTypeHeader res) -> log "Invalid content type header" res
-    (FailureResponse _ res)        -> (announce *> die) (reason res)
+    (FailureResponse _ res)        -> logId (reason res)
   where
     reason :: Response -> Text
     reason =
@@ -71,6 +86,9 @@ handleClientError clientError = case clientError of
             . decode @VaultFailureResponse
             . responseBody
 
+    logId :: Text -> Shell a
+    logId err = announce err >> die err
+
     log :: Text -> Response -> Shell a
     log err full = announce err >> die (err <> ":" <> tshow' (responseBody full))
 
@@ -78,10 +96,10 @@ tshow' :: LBS.ByteString -> Text
 tshow' = Text.pack . show
 
 askPassword :: Shell Password
-askPassword = Password . lineToText <$> dmenu args ""
+askPassword = Password . lineToText <$> inproc "dmenu" args ""
   where
     obscureColor = "#222222"
-    args = ["-p", "Password: ", "-nb", obscureColor, "-nf", obscureColor]
+    args = ["-p", "Enter Password ", "-nb", obscureColor, "-nf", obscureColor]
 
 login :: ClientEnv -> Password -> Shell UnlockData
 login env pw =
@@ -103,41 +121,35 @@ logout env =
   where
     lock = vaultClient // lockingEp // lockEp
 
-getItems :: ClientEnv -> Shell ItemsData
+getItems :: ClientEnv -> Shell [ItemTemplate]
 getItems env =
     liftIO (runClientM items env) >>= \case
         Left e -> handleClientError e
-        Right (VaultResponse res) -> pure res
+        Right (VaultResponse res) -> pure (coerce res)
   where
     items = vaultClient // itemsEp // getItemsEp
 
-dmenuSelect :: NonEmpty Line -> Shell Line
-dmenuSelect ls = dmenu ["-l", tshow (length ls)] (select ls)
+getStatus :: ClientEnv -> Shell StatusData
+getStatus env =
+    liftIO (runClientM status env) >>= \case
+        Left e -> handleClientError e
+        Right (VaultResponse res) -> pure res
+  where
+    status = vaultClient // miscEp // statusEp
 
 announce :: Text -> Shell ()
-announce msg = void $ dmenuSelect (textToLines msg)
-
-tshow :: (Show a) => a -> Text
-tshow = Text.pack . show
-
-getSessionKey :: FilePath -> IO (Maybe Text)
-getSessionKey fp = (Just <$> Text.IO.readFile fp) `catch` handler
-  where
-    handler :: IOException -> IO (Maybe Text)
-    handler _ = pure Nothing
-
-itemToLine :: ItemTemplate -> Line
-itemToLine = unsafeTextToLine . itName
+announce msg = void $ dmenuSelect [] (textToLines msg)
 
 type Todo = PostNoContent
 
 data VaultApi as = VaultApi
-    { lockingEp :: as :- NamedRoutes LockApi
+    { lockingEp :: as :- NamedRoutes VaultLockApi
     , itemsEp   :: as :- NamedRoutes VaultItemsApi
+    , miscEp    :: as :- NamedRoutes VaultMiscellaneousApi
     }
     deriving stock (Generic)
 
-data LockApi as = LockApi
+data VaultLockApi as = LockApi
     { lockEp :: as :- "lock" :> Post '[JSON] (VaultResponse LockData)
     , unlockEp :: as :- "unlock" :> ReqBody '[JSON] Password :> Post '[JSON] (VaultResponse UnlockData)
     }
@@ -202,6 +214,40 @@ data VaultItemsApi as = VaultItemsApi
     }
     deriving stock (Generic)
 
+newtype VaultMiscellaneousApi as = VaultMiscellaneousApi
+    { statusEp :: as :- "status" :> Get '[JSON] (VaultResponse StatusData)
+    }
+    deriving stock (Generic)
+
+data StatusData = StatusData
+    { statusDataServerUrl :: Maybe Text
+    , statusDataLastSync  :: UTCTime
+    , statusDataUserEmail :: Text
+    , statusDataUserId    :: Text
+    , statusDataStatus    :: LockStatus
+    }
+    deriving stock (Show, Eq)
+
+data LockStatus = Locked | Unlocked
+    deriving stock (Show, Eq)
+
+instance FromJSON LockStatus where
+    parseJSON = withText "LockStatus" $ \case
+        "locked" -> pure Locked
+        "unlocked" -> pure Unlocked
+        s -> fail ("Invalid lock status: " <> Text.unpack s)
+
+instance FromJSON StatusData where
+    parseJSON =
+        withObject "StatusData" $
+            (.: "template") >=> \t -> do
+                statusDataServerUrl <- t .: "serverUrl"
+                statusDataLastSync <- t .: "lastSync"
+                statusDataUserEmail <- t .: "userEmail"
+                statusDataUserId <- t .: "userId"
+                statusDataStatus <- t .: "status"
+                pure StatusData{..}
+
 newtype ItemsData = ItemsData
     { unItemsData :: [ItemTemplate]
     }
@@ -213,9 +259,30 @@ instance FromJSON ItemsData where
 data Item
     = ItemLogin Login
     | ItemCard Card
-    | ItemIdentity Identity
     | ItemSecureNote SecureNote
-    deriving stock (Show, Eq)
+    | ItemIdentity Identity
+    deriving stock (Show, Eq, Ord)
+
+toEmoji :: Item -> Text
+toEmoji = \case
+    ItemLogin _ -> "🔐"
+    ItemCard _ -> "💳"
+    ItemIdentity _ -> "👤"
+    ItemSecureNote _ -> "🗒️"
+
+instance ToLine ItemTemplate where
+    toLine (ItemTemplate _ name _ item) = merge $ mapMaybe textToLine $ mappend [toEmoji item, name] $ case item of
+        ItemLogin Login{..} -> [loginUsername] <> map unUri loginUris
+        ItemCard Card{..}   -> [Text.take 5 cardNumber <> "..."]
+        ItemIdentity _      -> ["Identity"]
+        ItemSecureNote _    -> ["Secure Note"]
+
+merge :: [Line] -> Line
+merge = mconcat . intersperse " - "
+
+safeHead :: [a] -> Maybe a
+safeHead []      = Nothing
+safeHead (x : _) = Just x
 
 data ItemTemplate = ItemTemplate
     { itfolderId :: Maybe Text
@@ -225,10 +292,13 @@ data ItemTemplate = ItemTemplate
     }
     deriving (Show, Eq)
 
+instance Ord ItemTemplate where
+    compare = comparing itItem
+
 newtype Uri = Uri
     { unUri :: Text
     }
-    deriving stock (Show, Eq)
+    deriving stock (Show, Eq, Ord)
 
 instance FromJSON Uri where
     parseJSON = withObject "Uri" $ \o -> Uri <$> o .: "uri"
@@ -238,16 +308,16 @@ data Login = Login
     , loginUsername :: Text
     , loginPassword :: Text
     }
-    deriving stock (Show, Eq)
+    deriving stock (Show, Eq, Ord)
 
 instance FromJSON Login where
     parseJSON = withObject "Login" $ \o ->
         Login <$> o .: "uris" <*> o .: "username" <*> o .: "password"
 
-data SecureNote = SecureNote
+newtype SecureNote = SecureNote
     { secureNoteType :: Int
     }
-    deriving stock (Show, Eq)
+    deriving stock (Show, Eq, Ord)
 
 instance FromJSON SecureNote where
     parseJSON = withObject "SecureNote" $ \o -> SecureNote <$> o .: "type"
@@ -257,14 +327,14 @@ data Card = Card
     , cardNumber   :: Text
     , cardCode     :: Text
     }
-    deriving stock (Show, Eq)
+    deriving stock (Show, Eq, Ord)
 
 instance FromJSON Card where
     parseJSON = withObject "Card" $ \o ->
         Card <$> o .: "cardholderName" <*> o .: "number" <*> o .: "code"
 
 data Identity = Identity {}
-    deriving stock (Show, Eq)
+    deriving stock (Show, Eq, Ord)
 
 instance FromJSON Identity where
     parseJSON = withObject "Identity" $ const (pure Identity)
